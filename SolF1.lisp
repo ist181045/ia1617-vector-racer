40dce1d6c3dfceb3c13902e39733444177f9de01
Rui Ventura <RGCV@users.noreply.github.com>
2016-10-20 23:42:49 +0100

    Finish implementing functions

--- SolF1.lisp
+++ SolF1.lisp
@@ -12,25 +12,59 @@
 ;(load "auxfuncs.fas")
 
 (defun isObstaclep (pos track)
-  (not
-		(nth (cadr pos) (nth (car pos) (track-env track)))
-	)
-)
+	(not
+		(nth (cadr pos) (nth (car pos) (track-env track)))))
 
 (defun isGoalp (st)
-  (= (state-cost st) -100)
-)
+	;(= (state-cost st) -100)
+	;(coordInList (state-pos st) (track-endpositions (state-track st)))
+	(not (null
+			(member
+				(state-pos st)
+				(track-endpositions (state-track st))
+				:test #'equal))))
+
+;(defun coordInList (coord lst)
+;	((if (not (null lst))
+;		(if (equal coord (car lst))
+;			t
+;			(coordInList coord (cdr lst))
+;		)
+;		nil
+;	))
+;)
 
 (defun nextState (st act)
 	(let* (
-			(v_t (list (+ (car (state-vel st)) (car act)) (+ (cadr (state-vel st)) (cadr act))))
-			(p_t (list (+ (car (state-pos st)) (car v_t)) (+ (cadr (state-pos st)) (cadr act))))
-			(state (make-state :pos p_t :vel v_t :action act :cost 1))
+			(v_t
+				(list
+					(+ (car (state-vel st)) (car act))
+					(+ (cadr (state-vel st)) (cadr act))))
+			(p_t
+				(list
+					(+ (car (state-pos st)) (car v_t))
+					(+ (cadr (state-pos st)) (cadr act))))
+			(state
+				(make-state
+					:pos p_t
+					:vel v_t
+					:action act
+					:cost 1
+					:other (state-other st)))
 		)
 		(cond
-			((isObstaclep p_t (state-track track)) ())
+			((isObstaclep p_t (state-track track))
+				(setf
+					(state-pos state) (state-pos st)
+					(state-vel state) '(0 0)
+					(state-cost state) 20)
+			)
+			((isGoalp state)
+				(state-cost state -100)
+			)
 			(t state)
 		)
+		state
 	)
 )
 ;  (make-STATE :POS '(3 16)
