ed2a2f10e8843691e6b0b73bdabd0f09f2496716
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2016-11-08 16:01:58 +0000

    Tabs -> spaces, CRLF -> LF and implement nextStates

--- stage_02/SolF2.lisp
+++ stage_02/SolF2.lisp
@@ -3,24 +3,24 @@
 
 
 ;;; TAI position
-(defun make-pos (c l)
-  (list c l))
+(defun make-pos (l c)
+  (list l c))
 (defun pos-l (pos)
   (first pos))
 (defun pos-c (pos)
   (second pos))
 
 ;;; TAI acceleration
-(defun make-acce (c l)
-  (list c l))
+(defun make-acce (l c)
+  (list l c))
 (defun acce-l (pos)
   (first pos))
 (defun acce-c (pos)
   (second pos))
 
 ;;; TAI velocity
-(defun make-vel (c l)
-  (list c l))
+(defun make-vel (l c)
+  (list l c))
 (defun vel-l (pos)
   (first pos))
 (defun vel-c (pos)
@@ -41,50 +41,50 @@
       (null (getTrackContent pos track))))
 
 ;; Pedir 0,4
-(defun isGoalp (st) 
+(defun isGoalp (st)
   "check if st is a solution of the problem"
   (let ((current-position (state-pos st))
-	(track (state-track st)))
+  (track (state-track st)))
     (and (member current-position (track-endpositions track) :test #'equalp)
-	 T)))
+   T)))
 
 ;; Pedir 1,2
 (defun nextState (st act)
   "generate the nextState after state st and action act from prolem"
   (let ((new-state (make-state :action act :track (state-track st))))
     (setf (state-vel new-state)
-	  (make-vel (+ (vel-l (state-vel st)) (acce-l act))
-		    (+ (vel-c (state-vel st)) (acce-c act))))
+    (make-vel (+ (vel-l (state-vel st)) (acce-l act))
+        (+ (vel-c (state-vel st)) (acce-c act))))
     (setf (state-pos new-state)
-	  (make-pos (+ (pos-l (state-pos st)) (vel-l (state-vel new-state)))
-		    (+ (pos-c (state-pos st)) (vel-c (state-vel new-state)))))
+    (make-pos (+ (pos-l (state-pos st)) (vel-l (state-vel new-state)))
+        (+ (pos-c (state-pos st)) (vel-c (state-vel new-state)))))
     (setf (state-cost new-state)
-	  (cond ((isGoalp new-state) -100)
-		((isObstaclep (state-pos new-state) (state-track new-state)) 20)
-		(T 1)))
+    (cond ((isGoalp new-state) -100)
+    ((isObstaclep (state-pos new-state) (state-track new-state)) 20)
+    (T 1)))
     (when (= (state-cost new-state) 20)
       (setf (state-vel new-state) (make-vel 0 0))
       (setf (state-pos new-state) (make-pos (pos-l (state-pos st))
-					    (pos-c (state-pos st)))))
+              (pos-c (state-pos st)))))
     (values new-state)))
 
 
 
 ;; Solution of phase 2
 
-;;; Pedir 
+;;; Pedir
 (defun nextStates (st)
-  "generate all possible next states"
-	(list st))
+  (loop for act in (possible-actions)
+    collect (nextState st act)))
 
-;;; limdepthfirstsearch 
+;;; limdepthfirstsearch
 (defun limdepthfirstsearch (problem lim &key cutoff?)
   "limited depth first search
      st - initial state
      problem - problem information
      lim - depth limit"
-	(list (make-node :state (problem-initial-state problem))) )
-				      
+  (list (make-node :state (problem-initial-state problem))) )
+
 
 ;iterlimdepthfirstsearch
 (defun iterlimdepthfirstsearch (problem &key (lim most-positive-fixnum))
@@ -92,5 +92,4 @@
      st - initial state
      problem - problem information
      lim - limit of depth iterations"
-	(list (make-node :state (problem-initial-state problem))) )
-
+  (list (make-node :state (problem-initial-state problem))) )
